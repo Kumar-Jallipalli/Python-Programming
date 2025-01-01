8d37cee3670a9f6f390fe2730da65c68ca90f5c6
Kumar Jallipalli <kumarjallipalli71@gmail.com>
2025-01-01 09:14:07 +0530

    Intro to Python

---
+++ 01_Intro/01Intro.py
@@ -0,0 +1,94 @@
+## INTRO
+'''
+    - Python is a widely used general-purpose, high level programming language
+    - Python was created by Guido van Rossum in 1991
+    - It was designed with an emphasis on code readability and its syntax allows to write concepts in fewer lines of code. → “Ease of Use”
+    - Python supports ”Multi-paradigm Approach”, like object-oriented, imperative and functional programming or procedural.
+    - There are two major Python versions: **`Python 2` and `Python 3`** Both are quite different.
+'''
+
+
+## Python Features
+'''
+    - **Interpreted**
+        - **No separate compilation and execution is required**
+        - Internally, Python converts the source code into an intermediate form called bytecodes
+            - which is then translated into native language of specific computer to run it.
+        - No need to worry about linking and loading with libraries, etc.
+    - **Platform Independent**
+    - **Robust**
+    - **Rich Library Support**
+'''
+
+
+## Interpreter
+'''
+- an interpreter is required to interpret and run our Python Programs.
+    - Online → Online interpreters like [**https://ide.geeksforgeeks.org/**](https://ide.geeksforgeeks.org/) 
+               can be used to run Python programs without installing an interpreter.
+    - Windows → IDLE (Integrated Development Environment) that comes bundled with the Python software downloaded 
+                from [**http://python.org/**](http://python.org/).
+'''
+
+## First Program
+print("Happy New Year 2025..!")
+'''
+    - To print something on the console, `print()` function is used
+    - This function also adds a newline after our message is printed(unlike in C)
+'''
+
+
+'''
+NOTE
+----
+    - In Python 2, “print” is not a function but a keyword and therefore can be used without parentheses.
+    - However, in Python 3, it is a function and must be invoked with parentheses.
+'''
+
+
+## Comments
+'''
+    - In Python Comments start with
+        - Single Line Comments → `#`
+        - Multi Line Comments → """ """
+    - Comments are Ignored by Interpreter
+    - Comments are used for
+        - Documenting the Code
+        - De-Bugging the Code
+'''
+
+
+## Python (vs) Java
+'''
+                Python                                                  Java
+                ------                                                  ----
+    Dynamically Typed Language                                  Statistically Typed Language
+    Uses Indentation for Structuring Code                       Uses Braces for Structuring Code
+    Concise & Compact                                           Verbose & Less Compact
+'''
+
+
+## Python Use Cases
+'''
+        1. Python has also been used in artificial intelligence
+        2. Python is often used for **natural language processing** tasks.
+        3. GNU Debugger uses Python as a **pretty printer** to show complex structures such as C++ containers.
+        4. Raspberry Pi– single board computer uses Python as its principal user-programming language
+        5. Python is now being used **Game Development** areas also.
+        6. A number of Linux distributions use installers written in Python example in Ubuntu we have the **Ubiquity**
+'''
+
+
+### **Organizations using Python :**
+'''
+        1. Google(Components of Google spider and Search Engine)
+        2. Yahoo(Maps)
+        3. YouTube
+        4. Mozilla
+        5. Dropbox
+        6. Microsoft
+        7. Cisco
+        8. Spotify
+        9. Quora
+'''
+
